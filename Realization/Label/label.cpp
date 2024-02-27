691ff11daccbacc370d0bf3e04c59a42aa1792a4
Amirka04 <kokovamir6@gmail.com>
2024-02-28 01:46:45 +0300

    Добавление нового функционала расширяющего базовые возможности класса sf::Text и исправление недочётов

--- Realization/Label/label.cpp
+++ Realization/Label/label.cpp
@@ -3,15 +3,37 @@
 
 
 
-Label::Label(const sf::String &msg, const sf::String &font_path, sf::Uint16 font_size) {
+Label::Label(const sf::String &msg, sf::Uint16 font_size, const sf::String &font_path) {
     setString(msg);
     setCharacterSize(font_size);
     font.loadFromFile(font_path);
     setFont(font);
+
+    def_color = {255, 255, 255, 255};
+    drawColor = focus_color = def_color;
 }
 
 
-void Label::update() {}
+bool Label::isFocus() {
+    if( Window::mouse.x >= getPosition().x && Window::mouse.y >= getPosition().y \
+        && Window::mouse.x <= getPosition().x + getGlobalBounds().width && Window::mouse.y <= getPosition().y + getGlobalBounds().height)
+        return true;
+    return false;
+}
+
+void Label::FocusColor(const sf::Color &color) {
+    focus_color = color;
+}
+
+
+
+void Label::update() {
+    drawColor = def_color;
+    if(isFocus() and focus_color != def_color)
+        drawColor = focus_color;
+
+    setFillColor(drawColor);
+}
 
 
 void Label::event(sf::Event) {}
@@ -23,5 +45,6 @@ void Label::draw(sf::RenderWindow& display) {
 
 
 Label::~Label() {
-
+    // будет производить автоматическое удаление себя же из общего массива
+    Window::erase_object(this);
 }
--- Realization/Label/label.h
+++ Realization/Label/label.h
@@ -7,19 +7,34 @@
 
 
 
-
 class Label : public Object, public sf::Text{
 public:
     // msg, font_file, size_font
-    explicit Label(const sf::String &msg, const sf::String &font_path = "fontGUI.ttf", sf::Uint16 font_size = 20);
+    explicit Label(const sf::String &msg, sf::Uint16 font_size = 20, const sf::String &font_path = "fontGUI.ttf");
+
+    // проверка на пересечение курсора и нашего текста
+    bool isFocus();
+
+    // установка значения при фокусе
+    void FocusColor(const sf::Color&);
+
+    // Деструктор
     ~Label() override;
+
 protected:
+    // базовые операции каждого объекта который будет отображаться на экране, будь то контейнер, состояние и т.п
     void update() override;
     void event(sf::Event event) override;
     void draw(sf::RenderWindow&) override;
 
+
+    sf::Color drawColor;
+    sf::Color def_color;
+    sf::Color focus_color;
+
     sf::Font font;
 };
+
 
 
 
