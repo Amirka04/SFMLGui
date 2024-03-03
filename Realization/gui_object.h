9771a8f2850052f0c47b876cbb02454391d93f0e
Amirka04 <kokovamir6@gmail.com>
2024-03-04 00:24:10 +0300

    Изменение деструктора, теперь он дефолтный

--- Realization/gui_object.h
+++ Realization/gui_object.h
@@ -14,7 +14,7 @@ class Object{
         virtual void draw(sf::RenderWindow&) = 0;
         virtual void update() = 0;
         virtual void event(sf::Event event) = 0;
-        virtual ~Object(){}
+        virtual ~Object() = default;
 };
 
 
